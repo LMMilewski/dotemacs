7704d18affe22eed10f97d9b56958e730db4f381
Łukasz Milewski <lmmilewski@gmail.com>
2011-06-03 00:32:22 +0200

    simplify PATH setting. set paths for otp/git

--- mac.el
+++ mac.el
@@ -1,7 +1,2 @@
-(let ((new-paths '("/usr/local/bin/"
-                   "/opt/local/bin/"
-                   "/Users/lmm/bin/"
-                   "/Users/lmm/otp/bin/")))
-  (setq exec-path (append new-paths exec-path))
-  (setenv "PATH" (concat (getenv "PATH") ":" (mapconcat 'identity new-paths ":"))))
-
+(add-exec-paths '("/usr/local/bin/"
+                  "/opt/local/bin/"))

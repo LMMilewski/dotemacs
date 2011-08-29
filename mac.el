b000a704fc5f5b92f17e9040f37f9e33d97b0f5d
Lukasz Milewski <lmmilewski@gmail.com>
2011-08-29 23:18:05 +0200

    add path to ~/bin

--- mac.el
+++ mac.el
@@ -1,3 +1,4 @@
 (add-exec-paths '("/usr/local/bin/"
                   "/opt/local/bin/"
-                  "/Users/lmm/libs/otp/bin/"))
+                  "/Users/lmm/libs/otp/bin/"
+                  "/Users/lmm/bin/"))
--- pc.el
+++ pc.el
@@ -1,2 +1,3 @@
 (add-exec-paths '("/home/lmm/apps/git/bin/"
-                  "/home/lmm/libs/otp/bin/"))
+                  "/home/lmm/libs/otp/bin/"
+                  "/home/lmm/bin"))

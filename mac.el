a273740cbfd9f7cbef9d5d1fcedfb33d5ffb5441
Lukasz Milewski <lmmilewski@gmail.com>
2011-10-01 00:52:14 +0200

    add paths to protobufs and pypy

--- mac.el
+++ mac.el
@@ -1,4 +1,6 @@
 (add-exec-paths '("/usr/local/bin/"
                   "/opt/local/bin/"
                   "/Users/lmm/libs/otp/bin/"
-                  "/Users/lmm/bin/"))
+                  "/Users/lmm/libs/protobuf/bin/"
+                  "/Users/lmm/bin/"
+                  "/Users/lmm/pypy/bin"))
